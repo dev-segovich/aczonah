0d682c4f92548f13bf8ec798d69b568e653502b8
chixchix55 <jsegovich55@gmail.com>
2024-03-10 12:44:58 -0400

    Modulacion y app
    Agregando la primera parte de la app modulada en una carpeta aparte

---
+++ app/header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Mar 10 2024 with Bootstrap v5.3.3
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+<header id="header" class="header fixed-top d-flex align-items-center">
+
+  <div class="d-flex align-items-center justify-content-between">
+    <a href="index.html" class="logo d-flex align-items-center">
+      <img src="assets/img/aczonah.png" alt="">
+      <!-- <span class="d-none d-lg-block">NiceAdmin</span> -->
+    </a>
+  </div><!-- End Logo -->
+  
+  <div class="d-flex align-items-center justify-content-center flex-grow-1">
+    <div class="card text-center" style="width: 80%; height: 100%; overflow-y: hidden;">
+      <div class="card-body d-flex align-items-center justify-content-center" style="height: 100%;">
+        <strong style="font-size: 19px;">Lugarena | Familia: Segovich</strong>
+      </div>
+    </div>
+</div><!-- End Centered Card -->
+
+
+  <nav class="header-nav ms-auto">
+    <ul class="d-flex align-items-center">
+
+      <div class="d-flex align-items-center justify-content-between">
+        <a href="index.html" class="logo d-flex align-items-center">
+          <img src="assets/img/cerrar.png" alt="">
+          <!-- <span class="d-none d-lg-block">NiceAdmin</span> -->
+        </a>
+      </div><!-- End Logo -->
+
+    </ul>
+  </nav><!-- End Icons Navigation -->
+
+  
+
+</header><!-- End Header -->
---
+++ app/index.php
@@ -0,0 +1,18 @@
+
+<?php include 'header.php'; ?>
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+    
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <?php include "footer.php"; ?>
+
+</body>
+
+</html>
